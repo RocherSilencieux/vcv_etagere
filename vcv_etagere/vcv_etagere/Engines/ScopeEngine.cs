94bc1e1abd6a277973e725ac63248281d7180d62
dimensi4 <lmarnette0@gmail.com>
2026-05-06 08:26:03 +0200

    update scope

--- vcv_etagere/vcv_etagere/Engines/ScopeEngine.cs
+++ vcv_etagere/vcv_etagere/Engines/ScopeEngine.cs
@@ -9,33 +9,11 @@ namespace vcv_etagere.Engines
         private int _writeIndex = 0;
         private IAudioNode? _input;
 
-        public float[] GetSamples()
-        {
-            lock (_samples)
-            {
-                return (float[])_samples.Clone();
-            }
-        }
+        
 
         public void SetInput(IAudioNode node) => _input = node;
         public void ClearInput() => _input = null;
 
-        public void WriteAudio(float[] buffer, int offset, int count)
-        {
-            if (_input != null)
-                _input.WriteAudio(buffer, offset, count);
-            else
-                Array.Clear(buffer, offset, count);
-
-            // Copy samples for display (take left channel)
-            lock (_samples)
-            {
-                for (int i = 0; i < count && i < BufferSize; i++)
-                {
-                    _samples[_writeIndex] = buffer[offset + i];
-                    _writeIndex = (_writeIndex + 1) % BufferSize;
-                }
-            }
-        }
+        
     }
 }
